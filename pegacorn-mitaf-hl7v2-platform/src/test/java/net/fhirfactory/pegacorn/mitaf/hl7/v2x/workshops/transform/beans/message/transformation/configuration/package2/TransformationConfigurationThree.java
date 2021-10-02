a4d8dcc3ca1baeb19a96aeba787129297470d47f
Brendan Douglas <brendan.douglas@act.gov.au>
2021-10-02 17:09:24 +1000

    Remove unused imports.

--- pegacorn-mitaf-hl7v2-platform/src/test/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/message/transformation/configuration/package2/TransformationConfigurationThree.java
+++ pegacorn-mitaf-hl7v2-platform/src/test/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/message/transformation/configuration/package2/TransformationConfigurationThree.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.BaseHL7MessageTransformationConfiguration;
-import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.Direction;
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.annotation.Egress;
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.annotation.MessageType;
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.annotation.RemoveHL7Segment;
