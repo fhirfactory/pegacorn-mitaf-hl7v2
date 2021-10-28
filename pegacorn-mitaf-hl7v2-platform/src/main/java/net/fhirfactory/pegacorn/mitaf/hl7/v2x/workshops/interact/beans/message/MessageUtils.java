b626e7dc8b7f64fa2253a7ca955bcadead902372
Brendan Douglas <brendan.douglas@act.gov.au>
2021-10-28 21:24:07 +1100

    Simplify the required segment annotation logic.  Process the message as a string rather than use the HL7 library.

--- pegacorn-mitaf-hl7v2-platform/src/main/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/interact/beans/message/MessageUtils.java
+++ pegacorn-mitaf-hl7v2-platform/src/main/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/interact/beans/message/MessageUtils.java
@@ -1,8 +1,11 @@
 package net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.interact.beans.message;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.Message;
 
 /**
@@ -26,5 +29,45 @@ public class MessageUtils {
 		 Matcher matcher = pattern.matcher(message.toString());
 		 
 		 return matcher.find();
+	}
+	
+	
+	/**
+	 * Removes all segments which are not in the required segment list
+	 * 
+	 * @param message
+	 * @param requiredSegments
+	 * @throws HL7Exception
+	 */
+	public static void removeNotRequiredSegments(Message message, List<String> requiredSegments) throws HL7Exception {		
+		String segments[] = message.toString().split("\r");
+		
+		List<Integer>indexesToKeep = new ArrayList<>();
+		
+		// If a segment needs to be kept adds its array index to the list of indexes to keep.
+		for (int i = 0; i < segments.length; i++) {
+			String segment = segments[i];
+			
+			for (String requiredSegment : requiredSegments) {
+				if (segment.startsWith(requiredSegment)) {
+					indexesToKeep.add(i);
+					break;
+				}
+			}
+		}
+		
+		
+		// Conntruct a new message from the array elements which have an index in the to keep list.
+		StringBuilder sb = new StringBuilder();
+		
+		for (Integer indexToKeep : indexesToKeep) {
+			if (sb.length() > 0) {
+				sb.append("\r");
+			}
+				
+			sb.append(segments[indexToKeep]);
+		}
+		
+		message.parse(sb.toString());
 	}
 }
--- pegacorn-mitaf-hl7v2-platform/src/main/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/message/transformation/configuration/step/HL7RequiredSegmentsTransformationStep.java
+++ pegacorn-mitaf-hl7v2-platform/src/main/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/message/transformation/configuration/step/HL7RequiredSegmentsTransformationStep.java
@@ -1,16 +1,13 @@
 package net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.message.transformation.configuration.step;
 
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ca.uhn.hl7v2.HL7Exception;
-import ca.uhn.hl7v2.model.AbstractGroup;
-import ca.uhn.hl7v2.model.Group;
 import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.model.Structure;
+import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.interact.beans.message.MessageUtils;
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.transformation.configuration.rule.Rule;
 import net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans.transformation.configuration.rule.TrueRule;
 
@@ -39,41 +36,7 @@ public class HL7RequiredSegmentsTransformationStep extends BaseMitafMessageTrans
 	public void process(Message message) throws HL7Exception {
 
 		if (rule.executeRule(message)) {
-				
-			// We need to get all the segments and what groups they are in.  We need this so we know what group to remove from.
-			Map<String, List<Group>> segmentInGroups = SegmentGroups.get(message);
-			
-			for (Map.Entry<String,List<Group>>entry : segmentInGroups.entrySet()) {
-				String name = entry.getKey();
-				
-				
-				// Allow all segments which start with one of the allowed segment names.
-				boolean matchFound = false;
-				
-				for (String allowedSegment : allowedSegmentCodes) {
-					if (name.startsWith(allowedSegment)) {
-						matchFound = true;
-						break;
-					}
-				}
-				
-				if (!matchFound) {
-				
-					// If we get here we have a segment to remove.  We need to remove from all groups.
-					for (Group groupWithSegment : entry.getValue()) {
-						Structure[] allSegmentsWithinGroup = groupWithSegment.getAll(name);
-											
-						for (int i = 0; i < allSegmentsWithinGroup.length; i++) {
-	
-							try {
-								((AbstractGroup)groupWithSegment).removeRepetition(name, i);
-							} catch(HL7Exception e) {
-								LOG.info("Attept to remove a segment which does not exist");
-							}
-						}
-					}
-				}
-			}
+			MessageUtils.removeNotRequiredSegments(message, allowedSegmentCodes);
 		}
 	}
 
