80919972901ecd0b14267eb448713621c2670e13
kelskye <kelskye@gmail.com>
2022-07-19 17:46:26 +1000

    249551 - Media Storage
    Converter for a raw HL7 Oru message into Fhir Media object and unit test that loads a ORU message with OBX segment

---
+++ pegacorn-mitaf-hl7v2-platform/src/main/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/HL7v2SnippetToFHIRMedia.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2021 ACT Health
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.camel.Exchange;
+import org.hl7.fhir.r4.model.Media;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v24.group.ORU_R01_OBSERVATION;
+import ca.uhn.hl7v2.model.v24.group.ORU_R01_ORDER_OBSERVATION;
+import ca.uhn.hl7v2.model.v24.group.ORU_R01_PATIENT_RESULT;
+import ca.uhn.hl7v2.model.v24.message.ORU_R01;
+import ca.uhn.hl7v2.model.v24.segment.OBX;
+import net.fhirfactory.pegacorn.internals.fhir.r4.resources.media.factories.MediaFactory;
+import net.fhirfactory.pegacorn.internals.hl7v2.interfaces.HL7v2xInformationExtractionInterface;
+
+@ApplicationScoped
+public class HL7v2SnippetToFHIRMedia {
+    private static final Logger LOG = LoggerFactory.getLogger(HL7v2SnippetToFHIRMedia.class);
+
+    @Inject
+    private MediaFactory mediaFactory;
+
+    @Inject
+    private HL7v2xInformationExtractionInterface messageInformationExtractionInterface;
+
+    @PostConstruct
+    public void initialise(){
+    }
+
+    public Media extractMediaResource(Message message, Exchange camelExchange){
+        LOG.debug(".extractMediaResource(): Entry, message->{}", message);
+        Media media = parseResource(message);
+        LOG.debug(".extractMediaResource(): Exit, media->{}", media);
+        return(media);
+    }
+
+	private Media parseResource(Message message) {
+        String messageID = messageInformationExtractionInterface.extractMessageID(message);
+        Date messageDate = messageInformationExtractionInterface.extractMessageDate(message);
+
+		Media media = mediaFactory.newMediaResource(messageID, messageDate);
+		byte[] data = extractMediaFromORU(message);
+		if(data == null) {
+			return null;
+		}
+		media.getContent().setData(data);
+		return media;
+	}
+
+	private byte[] extractMediaFromORU(Message message) {
+		try {
+			ORU_R01 oru = (ORU_R01) message;
+			ORU_R01_PATIENT_RESULT patientResult = oru.getPATIENT_RESULT();
+			ORU_R01_ORDER_OBSERVATION observationOrder = patientResult.getORDER_OBSERVATION();
+			ORU_R01_OBSERVATION observation = observationOrder.getOBSERVATION();
+			OBX obx = observation.getOBX();
+			LOG.trace("OBX: ->{}", obx.encode());
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < obx.getObservationValueReps(); i++) {
+				sb.append(obx.getObservationValue(i).encode());
+			}
+			LOG.trace("OBX output: ->{}", sb.substring(0));
+			return sb.substring(0).getBytes();
+		
+		} catch (ClassCastException e) {
+			LOG.warn("Not of the right type!");
+		} catch (HL7Exception e) {
+			LOG.warn("HL7 could not encode message ->{}", e.getMessage());
+		}
+		return null;
+	}
+    
+    @VisibleForTesting
+    void setMessageInformationExtractionInterface(HL7v2xInformationExtractionInterface messageInformationExtractionInterface) {
+    	this.messageInformationExtractionInterface = messageInformationExtractionInterface;
+    }
+    
+    @VisibleForTesting
+    void setMediaFactory(MediaFactory mediaFactory) {
+    	this.mediaFactory = mediaFactory;
+    }
+
+}
---
+++ pegacorn-mitaf-hl7v2-platform/src/test/java/net/fhirfactory/pegacorn/mitaf/hl7/v2x/workshops/transform/beans/HL7v2SnippetToFHIRMediaTest.java
@@ -0,0 +1,109 @@
+package net.fhirfactory.pegacorn.mitaf.hl7.v2x.workshops.transform.beans;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.hl7.fhir.r4.model.Media;
+import org.hl7.fhir.r4.model.Reference;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.Parser;
+import net.fhirfactory.pegacorn.core.constants.systemwide.DeploymentSystemSiteIdentificationInterface;
+import net.fhirfactory.pegacorn.core.constants.systemwide.PegacornReferenceProperties;
+import net.fhirfactory.pegacorn.internals.fhir.r4.codesystems.PegacornIdentifierCodeSystemFactory;
+import net.fhirfactory.pegacorn.internals.fhir.r4.internal.systems.DeploymentInstanceDetailInterface;
+import net.fhirfactory.pegacorn.internals.fhir.r4.resources.identifier.PegacornIdentifierFactory;
+import net.fhirfactory.pegacorn.internals.fhir.r4.resources.media.factories.MediaFactory;
+import net.fhirfactory.pegacorn.internals.hl7v2.helpers.HL7v2xMessageInformationExtractor;
+import net.fhirfactory.pegacorn.internals.hl7v2.interfaces.HL7v2xInformationExtractionInterface;
+
+public class HL7v2SnippetToFHIRMediaTest {
+	
+	private HL7v2SnippetToFHIRMedia converter;	
+	private HL7v2xInformationExtractionInterface messageExtractor;		
+	private PegacornReferenceProperties systemWideProperties;
+	private PegacornIdentifierFactory identifierFactory;
+	private PegacornIdentifierCodeSystemFactory pegacornIdentifierCodeSystemFactory;	
+	private MediaFactory mediaFactory;
+    private Parser pipeParser;
+	
+	@SuppressWarnings("resource")
+	@BeforeEach
+	public void setup() {
+		pipeParser = new DefaultHapiContext().getPipeParser();
+        pipeParser.getParserConfiguration().setValidating(false);
+        pipeParser.getParserConfiguration().setEncodeEmptyMandatoryFirstSegments(true);
+
+		converter = new HL7v2SnippetToFHIRMedia();
+		messageExtractor = new HL7v2xMessageInformationExtractor();
+		converter.setMessageInformationExtractionInterface(messageExtractor );
+		mediaFactory = new MediaFactory();
+		identifierFactory = new PegacornIdentifierFactory();
+		pegacornIdentifierCodeSystemFactory = new PegacornIdentifierCodeSystemFactory();
+		systemWideProperties = new PegacornReferenceProperties();
+		pegacornIdentifierCodeSystemFactory.setSystemWideProperties(systemWideProperties);
+		identifierFactory.setPegacornIdentifierCodeSystemFactory(pegacornIdentifierCodeSystemFactory);
+		mediaFactory.setIdentifierFactory(identifierFactory);
+		converter.setMediaFactory(mediaFactory );
+		identifierFactory.setSiteIdentification(new DeploymentSystemSiteIdentificationInterface() { @Override public String getDeploymentSite() { return ""; } } );
+		identifierFactory.setPegacornReferenceProperties(systemWideProperties);
+		DeploymentInstanceDetailInterface deploymentDetails = new DeploymentInstanceDetailInterface() {		
+			@Override
+			public Reference getDeploymentInstanceSystemOwnerPractitionerRole() { return null; }
+			@Override
+			public Reference getDeploymentInstanceSystemOwnerPractitioner() { return null; }
+			@Override
+			public Reference getDeploymentInstanceSystemOwnerOrganization() { return null; }
+			@Override
+			public String getDeploymentInstanceSystemOwnerContactName() { return null; }
+			@Override
+			public String getDeploymentInstanceSystemEndpointSystem() { return null; }
+			@Override
+			public Reference getDeploymentInstanceSystemAdministratorPractitionerRole() { return null; }
+			@Override
+			public Reference getDeploymentInstanceSystemAdministratorPractitioner() { return null; }
+			@Override
+			public String getDeploymentInstanceSystemAdministratorContactName() { return null; }
+			@Override
+			public String getDeploymentInstanceOrganizationName() { return null; }
+			@Override
+			public Reference getDeploymentInstanceOrganization() {return null;}
+		};
+		identifierFactory.setDeploymentInstanceDetailInterface(deploymentDetails );
+	}
+	
+	@Test
+	void testExtractMediaResource() {
+		try {
+			Message resource = loadORUResource();
+			Media media = converter.extractMediaResource(resource, null);
+			Assert.assertNotNull(media);
+			Assert.assertNotNull(media.getIdentifierFirstRep());
+			Assert.assertNotNull(media.getContent().getData());
+		} catch (IOException e) {
+			fail(e);
+		}
+	}
+	private Message loadORUResource() throws IOException {
+		Path filePath = Path.of("./src/test/resources/oru_r01_wth_attachment.txt");
+
+		String content = Files.readString(filePath);
+		Message hl7Msg;
+		try {
+			hl7Msg = pipeParser.parse(content);
+			return hl7Msg;
+		} catch (HL7Exception e) {
+			e.printStackTrace();
+		}
+        
+		return null;
+	}
+}
